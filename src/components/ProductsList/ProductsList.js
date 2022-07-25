45a333a3629f5f25ae0681ad070e52d5c991a2d4
boyuanjiang0.0 <boyuanjiang0.0@gmail.com>
2022-07-25 17:01:24 +1000

    feat(productionsList) : filter function

--- src/components/ProductsList/ProductsList.js
+++ src/components/ProductsList/ProductsList.js
@@ -3,59 +3,58 @@ import PropTypes from "prop-types";
 import styles from "./ProductsList.module.scss";
 import { Select, Card } from "antd";
 
-import products_json from '../assets/products_json.json'
+import products_json from "../assets/products_json.json";
 
 const { Meta } = Card;
 const ProductsList = () => {
-
-  // state 
+  // state
   const [products, setProducts] = React.useState(products_json);
 
   const { Option } = Select;
   const handleChange = (value) => {
     console.log(`selected ${value}`);
+    if (value == "All") {
+      setProducts(products_json);
+    } else {
+      setProducts(products_json.filter((x) => x.type == value));
+    }
   };
   return (
     <div className={styles.ProductsList} data-testid="ProductsList">
-      ProductsList Component
-
       <div className={styles.filter_container}>
         <p className={styles.filter_container_title}>Filter by</p>
         <Select
           defaultValue="All"
           style={{
             width: 120,
-            marginRight: '30px'
+            marginRight: "30px",
           }}
           onChange={handleChange}
         >
           <Option value="All">All</Option>
           <Option value="Beer">Beer</Option>
-          <Option value="Winde">Winde</Option>
+          <Option value="Wine">Wine</Option>
           <Option value="Spirits">Spirits</Option>
           <Option value="Cider">Cider</Option>
         </Select>
       </div>
-
-
       <div className={styles.products_container}>
         {products.map((producct) => {
           return (
             <Card
               hoverable
               className={styles.product}
-              cover={<img alt={producct.productName} src={producct.productImage} />}
+              cover={
+                <img alt={producct.productName} src={producct.productImage} />
+              }
             >
               <div className={styles.sale}>sale </div>
               <Meta title={producct.productName + " : " + producct.price} />
             </Card>
-          )
+          );
         })}
-
-
-
       </div>
-    </div >
+    </div>
   );
 };
 
